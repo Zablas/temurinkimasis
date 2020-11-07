9ff1d58d1f9f2756dd2302e6a82b58f9ef432224
Zablas <vagabaliukas@gmail.com>
2020-11-07 16:09:59 +0200

    Added the initial design

--- resources/views/temos/create.blade.php
+++ resources/views/temos/create.blade.php
@@ -5,14 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Registruotis') }}</div>
+                <div class="card-header">{{ __('Sukurti naują temą') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Vardas') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Pavadinimas') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,11 +26,17 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('El. pašto adresas') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Aprašas') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                                <textarea id="aprasas" type="text"
+                                          class="form-control @error('name') is-invalid @enderror"
+                                          style="overflow: auto; resize: none"
+                                          name="aprasas"
+                                          value="{{ old('aprasas') }}"
+                                          required autocomplete="aprasas"
+                                          rows="10"
+                                          autofocus></textarea>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -39,32 +45,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Slaptažodis') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Patvirtinti slaptažodį') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Registruotis') }}
+                                    {{ __('Pridėti') }}
                                 </button>
                             </div>
                         </div>
